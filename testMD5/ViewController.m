04dec651f96ccf7c6efea91a25d891249c6b4146
yanxuegan <yanxuegan0903@163.com>
2017-05-05 11:08:46 +0800

    可以计算组合了  但是没有优化内存

--- testMD5/ViewController.m
+++ testMD5/ViewController.m
@@ -10,13 +10,57 @@
 
 @interface ViewController ()
 
+@property (nonatomic, strong) NSArray<NSString *> *charArray;
+
+@property (nonatomic, strong) NSMutableArray<NSString *> *MD5Array;
+
 @end
 
 @implementation ViewController
 
+- (NSArray<NSString *> *)charArray{
+    if (!_charArray) {
+        _charArray = @[@"0",@"1",@"2",@"3",@"4",@"5",@"6",@"7",@"8",@"9"];
+    }
+    return _charArray;
+}
+
+
+- (NSMutableArray<NSString *> *)MD5Array{
+    if (!_MD5Array) {
+        _MD5Array = [NSMutableArray arrayWithArray:self.charArray];
+    }
+    return _MD5Array;
+}
+
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    
+
+    NSMutableArray * array = [NSMutableArray arrayWithArray:self.charArray];
+    int bit = 8;
+    int start = 0;
+    int charCount = (int)self.charArray.count;
+    NSString * str ;
+    NSMutableString * str1;
+    for (int c = 0; c < bit - 1; c ++) {
+        start = c>=1?  (charCount)*((1-pow(charCount, c))/(1-charCount)) : 0;
+        for (int i = start; i < self.MD5Array.count; i++) {
+            str = self.MD5Array[i];
+            for (int j = 0; j < self.charArray.count; j++) {
+                str1 = [NSMutableString stringWithString:self.charArray[j]];
+                [str1 appendString:str];
+                [array addObject:str1];
+            }
+        }
+        [self.MD5Array removeAllObjects];
+        [self.MD5Array addObjectsFromArray:array];
+    }
+    NSLog(@"final MD5Array.count = %lu",(unsigned long)self.MD5Array.count);
+
+
+    
 }
 
 
